4c81e3dc15364dac5dea90f2853b4c6635c37350
Ramprasad GK <gkraam@gmail.com>
2020-02-02 07:10:06 +0000

    tc

--- vars/prime.groovy
+++ vars/prime.groovy
@@ -1,8 +1,12 @@
 @Grab('org.apache.commons:commons-math3:3.4.1')
 import org.apache.commons.math3.primes.Primes
 void parallelize(int count) {
+  println "begin"
   if (!Primes.isPrime(count)) {
     error "${count} was not prime"
   }
-  
+  else{
+      error "its a primr"
+  }
+  return "done"
 }
